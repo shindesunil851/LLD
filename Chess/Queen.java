31f7737593d3cbb778547d38a4183560645d09a7
sunil shinde <shindesunil851@gmail.com>
2024-07-24 02:17:48 +0530

    Create Queen.java

---
+++ Chess/Queen.java
@@ -0,0 +1,9 @@
+public class Queen extends Piece {
+    @Override
+    public boolean canMove(Board board, Block startBlock, Block endBlock) {
+        return false;
+    }
+    public Queen(boolean white) {
+        super(white);
+    }
+}
