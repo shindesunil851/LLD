b9da7aee8a71f25b2fca1e5a4067faa172bbffce
sunil shinde <shindesunil851@gmail.com>
2024-07-24 02:17:21 +0530

    Create King.java

---
+++ Chess/King.java
@@ -0,0 +1,8 @@
+public class King extends Piece {
+    public King(boolean white) {
+        super(white);
+    }
+    public boolean canMove(Board board, Block startBlock, Block endBlock) {
+        return false;
+    }
+}
